818cd0993371090c3b601be9c5ed785e3901f228
Hélèna Thomassin <thomassin.helena@gmail.com>
2019-10-27 21:54:08 +0100

    ajout squelette

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Liste des courses</h1>
       </header>
+    <div className="input">
+      
+        <input type="text" placeholder="un achat"/>
+      <button>+</button>
+    </div>
+    <div className="items-list">
+      <ul>
+        <li>lait</li>
+        <li>cereales</li>
+        
+      </ul>
+    </div>
+
     </div>
   );
 }
